32dc5419febb690c891c4c06b9c6572888fb87af
Moustafa <moustafaadel097@gmail.com>
2024-12-13 13:38:10 +0200

    Netlify

--- sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Fetching latest changes from origin yllaaa ..."
-git fetch origin
-git pull origin main
-
-echo "Pushing changes to deploy MOUSTAFA ..."
-git push deploy main --force
-
-echo "Sync complete!"
