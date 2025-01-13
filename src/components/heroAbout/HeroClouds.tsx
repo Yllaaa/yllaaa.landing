d6702d29a76c1c66cf1a376019553bd1773123e8
Moustafa <moustafaadel097@gmail.com>
2025-01-13 18:00:50 +0200

    Spelling checks

--- src/components/heroAbout/HeroClouds.tsx
+++ src/components/heroAbout/HeroClouds.tsx
@@ -4,13 +4,21 @@ import styles from "./heroClouds.module.css";
 import Image from "next/image";
 // import { getTranslations } from "next-intl/server";
 import { useTranslations } from "next-intl";
+import Loading from "@/app/[locale]/loading";
 // async
 function HeroClouds() {
   const t = useTranslations("cloudHero");
+  const [loaded, setLoaded] = React.useState(false);
   return (
     <>
+      {/* {!loaded && <Loading />} */}
+
       <div className={styles.container}>
-        <div className={styles.cloudsSection}>
+        <div
+          className={`${styles.cloudsSection} ${
+            loaded && styles.cloudsSectionAnimation
+          } `}
+        >
           <Image
             className={styles.cloud1}
             src={`https://yllaaa.s3.ca-central-1.amazonaws.com/Clouds.png`}
@@ -20,43 +28,54 @@ function HeroClouds() {
             loading="lazy"
             decoding="async"
             onWaiting={(e) => e.preventDefault()}
+            onLoadingComplete={() => setLoaded(true)}
           />
-          <Image
-            className={styles.cloud2}
-            src={`https://yllaaa.s3.ca-central-1.amazonaws.com/clouds01.png`}
-            alt="clouds"
-            width={1000}
-            height={1000}
-            loading="lazy"
-            decoding="async"
-            onWaiting={(e) => e.preventDefault()}
-          />
-          <Image
-            className={styles.cloud3}
-            src={`https://yllaaa.s3.ca-central-1.amazonaws.com/clouds02.png`}
-            alt="clouds"
-            width={1000}
-            height={1000}
-            loading="lazy"
-            decoding="async"
-            onWaiting={(e) => e.preventDefault()}
-          />
+          {loaded && (
+            <Image
+              className={styles.cloud2}
+              src={`https://yllaaa.s3.ca-central-1.amazonaws.com/clouds01.png`}
+              alt="clouds"
+              width={1000}
+              height={1000}
+              loading="lazy"
+              decoding="async"
+              onWaiting={(e) => e.preventDefault()}
+            />
+          )}
+          {loaded && (
+            <Image
+              className={styles.cloud3}
+              src={`https://yllaaa.s3.ca-central-1.amazonaws.com/clouds02.png`}
+              alt="clouds"
+              width={1000}
+              height={1000}
+              loading="lazy"
+              decoding="async"
+              onWaiting={(e) => e.preventDefault()}
+            />
+          )}
         </div>
-        <div className={styles.pyramidsSection}>
-          <Image
-            src={`https://yllaaa.s3.ca-central-1.amazonaws.com/pyramids.jpeg`}
-            alt="pyramids"
-            width={1000}
-            height={1000}
-            loading="lazy"
-            decoding="async"
-          />
-        </div>
-        <div className={styles.textSection}>
-          <h6>{t("yllaaa")}</h6>
-          <h4>{t("title")}</h4>
-          <p>{t("description")}</p>
-        </div>
+        {!loaded ? (
+          <Loading />
+        ) : (
+          <>
+            <div className={styles.pyramidsSection}>
+              <Image
+                src={`https://yllaaa.s3.ca-central-1.amazonaws.com/pyramids.jpeg`}
+                alt="pyramids"
+                width={1000}
+                height={1000}
+                loading="lazy"
+                decoding="async"
+              />
+            </div>
+            <div className={styles.textSection}>
+              <h6>{t("yllaaa")}</h6>
+              <h4>{t("title")}</h4>
+              <p>{t("description")}</p>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
