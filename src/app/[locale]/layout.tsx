faf3aadb71214a770fca410c26acc73a52b34330
Moustafa <moustafaadel097@gmail.com>
2025-08-21 08:30:34 +0300

    updates

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,24 +1,33 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
-import Footer from "@/components/zFooter/Footer";
-import Navbar from "@/components/aaaNavbar/Navbar";
+import { NextIntlClientProvider } from "next-intl"
+import { getMessages } from "next-intl/server"
+import Footer from "@/components/zFooter/Footer"
+import Navbar from "@/components/aaaNavbar/Navbar"
+import notFound from "../not-found"
+import "../globals.css"
 
 export default async function RootLayout({
   children,
   params,
 }: {
-  children: React.ReactNode;
-  params: { locale: any }; // Use a more specific type instead of `any`
+  children: React.ReactNode
+  params: { locale: any } // Use a more specific type instead of `any`
 }) {
+  const locales = ["en", "es", "ca", "ar"]
+
   // Destructure `params` to access `locale`
-  const { locale } = await params;
+  const { locale } = await params
+
+  if (!locales.includes(locale)) {
+    notFound()
+  }
 
   // Fetch messages server-side
-  const messages = await getMessages(locale);
+  const messages = await getMessages(locale)
+  const direction = locale === "ar" ? "rtl" : "ltr"
 
   return (
-    <section lang={locale}>
+    <section lang={locale} dir={direction}>
       <NextIntlClientProvider messages={messages}>
         <header>
           <Navbar />
@@ -29,5 +38,5 @@ export default async function RootLayout({
         </div>
       </NextIntlClientProvider>
     </section>
-  );
+  )
 }
